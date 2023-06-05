dd9e863c9c48aeb42af884e5d2c5aa1775eec731
NoNameCfg <65350561+NoNameCfg@users.noreply.github.com>
2023-06-05 23:47:42 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,37 @@
+fx_version 'adamant'
+game 'gta5'
+
+author "F1"
+description "https://discord.gg/Nb23tC3wmA"
+version "4.1"
+
+client_script 'main.lua'
+
+files {
+    'index.html',
+    'js/main.js',
+    'js/config.js',
+    'audio/*.mp3',
+    'audio/*.ogg',
+    'css/styles.css',
+    'img/*.jpg',
+    'img/*.png',
+    'img/*.gif',
+    'img/*.jpeg',
+    'video/*.mp4',
+    'video/video.mp4',
+    'img/*',
+    'img/*/*',
+}
+loadscreen_manual_shutdown 'yes'
+loadscreen 'index.html'
+loadscreen_cursor 'yes'
+
+
+--[[
+  --------------------------------------------------------------------------------------------------------------------------------------------------
+
+                                              THANKS FOR USE MY LOADING SCREEN JOIN THE DISCORD PLEASE :(
+
+  --------------------------------------------------------------------------------------------------------------------------------------------------
+--]]
